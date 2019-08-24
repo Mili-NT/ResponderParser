880c1b45007d234ea2f489e907361d8497a37de2
Mili <52385020+Mili-NT@users.noreply.github.com>
2019-08-24 17:21:10 -0400

    Added features
    Added protection against empty dicts so different Responder commands wont crash the program
    Added a Hash By Domain graph

--- ResponderParser.py
+++ ResponderParser.py
@@ -20,7 +20,7 @@ def session_file_proc(filename, folder):
         with open(filename, 'r') as logfile:
             for line in logfile.readlines():
                 cleaned = line.rstrip()[timestamp:]
-                if cleaned[1] == '*' and '[NBT-NS]' in cleaned:
+                if cleaned[4:12] == '[NBT-NS]':
                     workstring = cleaned.replace(' ', '').strip(lineinfo)[20:]
                     service_name = workstring.split('(service:')[1].replace(')', '')
                     if service_name not in service_dict.keys():
@@ -53,16 +53,26 @@ def session_file_proc(filename, folder):
                             UA_dict[user_agent] = 1
                     elif cleaned[7:18] == 'NTLMv2 Hash':
                         ntlm = cleaned.split(': ')[1]
-                        username = ntlm.split('::')
-                        if username[0] not in hash_dict.keys():
-                            hash_dict[username[0]] = username[1]
+                        username = ntlm.split(':')[2]
+                        if username not in hash_dict.keys():
+                            hash_dict[username] = 1
+                        else:
+                            hash_dict[username] += 1
     with open(f'{folder}ResponderSession_OS_{datetime.datetime.now().date()}.txt', 'w') as osfile:
         for k,v in OS_dict.items():
             osfile.write(f'OS Version: {k}, Instances: {v}\n')
     with open(f'{folder}ResponderSession_UA_{datetime.datetime.now().date()}.txt', 'w') as uafile:
         for k,v in UA_dict.items():
             uafile.write(f'User-Agent: {k}, Instances: {v}\n')
-    masterdict = {'osd':OS_dict, 'uad':UA_dict, 'nbsd':service_dict}
+    empty_list = [] # yes, this is hacky
+    masterdict = {'osd':OS_dict, 'uad':UA_dict, 'nbsd':service_dict, 'hsd': hash_dict}
+    for k in masterdict.keys():
+        if masterdict[k]:
+            pass
+        else:
+            empty_list.append(k)
+    for d in empty_list:
+        del masterdict[d]
     return masterdict
 def session_data_visualization(identifier, resources, folderpath):
     if identifier == 'osd':
@@ -74,6 +84,9 @@ def session_data_visualization(identifier, resources, folderpath):
     elif identifier == 'nbsd':
         savename = 'NBTServicesGraph'
         title = 'Net BIOS Services Prevalence'
+    elif identifier == 'hsd':
+        savename = 'HashGraph'
+        title = 'Hashes By Domain Graph'
     empty = []
     labels = []
     data = []
@@ -83,11 +96,9 @@ def session_data_visualization(identifier, resources, folderpath):
         data.append(resources[k])
     figureObject, axesObject = plotter.subplots()
     axesObject.pie(data, labels=empty, autopct='%1.2f', startangle=90)
-    if identifier == 'osd':
-        plotter.legend(title=title, loc="best", labels=labels)
-    elif identifier == 'uad':
+    if identifier == 'uad':
         plotter.legend(title=title, loc="lower center", labels=labels)
-    elif identifier == 'nbsd':
+    else:
         plotter.legend(title=title, loc="best", labels=labels)
     axesObject.axis('equal')
     plotter.savefig(f"{folderpath}{savename}")
