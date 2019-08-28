1a2795a95d37807820bd4c3342c6f3aec0b9610b
Mili <52385020+Mili-NT@users.noreply.github.com>
2019-08-28 16:10:29 -0400

    Concatenation and hash file renaming
    New features:
    
    ResponderParser now allows the user to concatenate multiple log files and generate graphs and data from that concatenated logfile.
    
    It also now outputs all hashes as an easily sortable [domain][username]Hash.txt format.

--- ResponderParser.py
+++ ResponderParser.py
@@ -1,25 +1,63 @@
 import re
 import matplotlib.pyplot as plotter
-from os import getcwd, mkdir
-from os.path import isdir, isfile
+from os import getcwd, mkdir, listdir
+from os.path import isdir
 import datetime
 #
 # Made by: Mili
 # Python Version: 3.6.0
 # Credit to Laurent Gaffie for making and maintaining Responder
+
+
+def concatenate():
+    save = input("Enter the path to save output to (include name and extension): ")
+    filelist = []
+    while True:
+        filepaths_input = input("Enter the filepath to the folder containing ONLY session logs: ")
+        if isdir(filepaths_input) is False:
+            print("Invalid Filepath.")
+            continue
+        else:
+            for file in listdir(filepaths_input):
+                truepath = filepaths_input + '\\' + file
+                filelist.append(truepath)
+            break
+    output = ''
+    for file in filelist:
+        with open(file, 'r') as f:
+            for line in f.readlines():
+                if 'Responder Started: ' in line:
+                    pass
+                else:
+                    output += line
+    with open(save, 'w+') as s:
+        s.write(output)
+    return save
 def session_file_proc(filename, folder):
+    def hash_parse(path):
+        with open(path, 'r') as file:
+            for l in file.readlines():
+                if '[HTTP] NTLMv2 Hash' in l:
+                    clean = l.rstrip()[25:]
+                    unsplit_hash = clean[len('[HTTP] NTLMv2 Hash     : '):]
+                    user = unsplit_hash.split(':')[0]
+                    domain = unsplit_hash.split('::')[1].split(':')[0]
+                    savename = f'[{domain}][{user}]Hash.txt'
+                    with open(f'{folder}{savename}', 'w') as sfile:
+                        sfile.write(unsplit_hash)
+                        sfile.close()
+
     netbios_dict = {}
     service_dict = {}
     OS_dict = {}
     UA_dict = {}
     hash_dict = {}
-    timestamp = len('08/23/2019 05:20:13 PM - ')
     lineinfo = '[*] [NBT-NS] '
     pattern = r'(?:\d{1,3}\.)+(?:\d{1,3})'
     with open(f'{folder}ResponderSession_Boxes_{datetime.datetime.now().date()}.txt', 'w') as savefile:
         with open(filename, 'r') as logfile:
             for line in logfile.readlines():
-                cleaned = line.rstrip()[timestamp:]
+                cleaned = line.rstrip()[25:]
                 if cleaned[4:12] == '[NBT-NS]':
                     workstring = cleaned.replace(' ', '').strip(lineinfo)[20:]
                     service_name = workstring.split('(service:')[1].replace(')', '')
@@ -73,6 +111,8 @@ def session_file_proc(filename, folder):
             empty_list.append(k)
     for d in empty_list:
         del masterdict[d]
+
+    hash_parse(filename)
     return masterdict
 def session_data_visualization(identifier, resources, folderpath):
     if identifier == 'osd':
@@ -109,16 +149,30 @@ def sessions_main():
         pass
     else:
         mkdir(folder)
+
     while True:
-        try:
-            fileinput = input("Enter the filepath of the Responder-Session log file: ")
+        catinput = input("[c]oncatenate multiple log files or [p]roceed to parser: ")
+        if catinput.lower() == 'c':
+            logfile = concatenate()
+            masterdict = session_file_proc(logfile, folder)
+            for k in masterdict.keys():
+                session_data_visualization(k, masterdict[k], folder)
             break
-        except FileNotFoundError:
-            print("No such file found.")
+        elif catinput.lower() == 'p':
+            while True:
+                try:
+                    fileinput = input("Enter the filepath of the Responder-Session log file: ")
+                    break
+                except FileNotFoundError:
+                    print("No such file found.")
+                    continue
+            masterdict = session_file_proc(fileinput, folder)
+            for k in masterdict.keys():
+                session_data_visualization(k, masterdict[k], folder)
+            break
+        else:
+            print("Invalid Input")
             continue
-    masterdict = session_file_proc(fileinput, folder)
-    for k in masterdict.keys():
-        session_data_visualization(k, masterdict[k], folder)
 
 if __name__ == '__main__':
     sessions_main()
